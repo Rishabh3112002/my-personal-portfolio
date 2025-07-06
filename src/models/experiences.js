9456902b75c99aa10904b746349166d38f908cc9
Rishabh Kathiravan <ksrsk2002@gmail.com>
2025-07-06 15:45:22 +1000

    Added new experience and projects

--- src/models/experiences.js
+++ src/models/experiences.js
@@ -7,8 +7,6 @@ const experiences = [
       "Built an interactive data orchestration pipeline to structure and embed unstructured data for ML models.",
       "Automated web scraping and standardized property data into JSON for consistent downstream processing.",
       "Developed a PyTorch-based model to segment floor plans into rooms and structural components.",
-      "Trained a transformer-based model to vectorize floor plans for property similarity comparisons.",
-      "Created automated triplet dataset generation scripts for training vectorization models.",
     ],
       duration: "March 2022 - June 2022",
   },
